74245a8ee6676b89f0d0ebb002af5199c572c7df
Sadaf <sadaf_cu@hotmail.com>
2019-03-22 10:42:54 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/keep-it/precache-manifest.661897923f96092a58e6ab580adeb403.js"
+  "/keep-it/precache-manifest.62fa5ed1532ea0f8926bc25f07f32ed9.js"
 );
 
 workbox.clientsClaim();
